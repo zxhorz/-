152031c487786211a4a572cedadcad288a3fe603
Xihao Zhou <xihaozhou@hengtiansoft.com>
2019-01-21 16:52:19 +0800

    update

--- design/dormMG/src/main/resources/static/left-menu-module/left.menu.directive.js
+++ design/dormMG/src/main/resources/static/left-menu-module/left.menu.directive.js
@@ -10,6 +10,22 @@
         };
 
         function leftMenuController($scope, $http, $location, $rootScope,$attrs){
+            angular.element('.menu .item');
+//            var element = angular.element('.menu .item');
+//            angular.element('.menu .item').bind('click',function() {
+//                if (jQuery(this).hasClass("active")) {
+//                    preventDefault();
+//                }
+//                if (!jQuery(this).hasClass("active")) {
+//                    jQuery("ul", jQuery(this).parents("ul:first")).slideUp(350);
+//                    jQuery(this).siblings().removeClass("active");
+//                    jQuery(this).find("ul").slideDown(350);
+//                    jQuery(this).addClass("active");
+//                } else if (jQuery(this).hasClass("active")) {
+//                    jQuery("ul", jQuery(this).parents("ul:first")).slideUp(350);
+//                    jQuery(this).removeClass("active");
+//                }
+//            });
         }
     }]);
 
