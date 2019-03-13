5e3134e2b6aea9dd27d5413e1b8fe6d469efb479
zxhorz <zhouxinhao666@gmai.com>
2019-03-13 22:47:28 +0800

    notice event
    notice event

--- design/dormMG/src/main/resources/static/notice-module/notice.controller.js
+++ design/dormMG/src/main/resources/static/notice-module/notice.controller.js
@@ -2,12 +2,18 @@
 	'use strict';
 	var app = angular.module('noticeModule',[])
 	    .controller('noticeController', ['$scope',function ($scope) {
-	    var data = [];
+//	    var data = [];
 
-//	    $('#tableEmailsList').DataTable(data);
+	    var table = $('#tableEmailsList').DataTable();
 //        var tableEmailsList = $('#tableEmailsList').DataTable();
 //        tableEmailsList.ajax.url = "/data/noticeList";
 //        tableEmailsList.ajax.reload();
+        $('#tableEmailsList tbody').on('click', 'tr', function () {
+            var data = table.row( this ).data();
+//            alert( 'You clicked on '+data[0]+'\'s row' );
+            openModal("/xx",{},"title",200,5,5  )
+        } );
+
 
 	}]);
 
